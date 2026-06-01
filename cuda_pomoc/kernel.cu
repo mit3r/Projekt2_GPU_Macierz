0d124ef051b15da000c8250cffe30994af5120db
mit3r <franek.kaczmarek45@gmail.com>
2026-06-01 16:32:50 +0200

    init

---
+++ cuda_pomoc/kernel.cu
@@ -0,0 +1,155 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <time.h>
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void addKernel(int *c, const int *a, const int *b)
+{
+    int i = threadIdx.x;
+    atomicAdd(&c[i], a[i] + b[i]);
+}
+
+int main()
+{
+    const int arraySize = 1000;
+     int a[arraySize];
+     int b[arraySize];  
+    for (int i = 0; i < arraySize; i++) a[i] = i;
+    for (int i = 0; i < arraySize; i++) b[i] = i;
+
+    int c[arraySize] = { 0 };
+    
+
+    // Add vectors in parallel.
+    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addWithCuda failed!");
+        return 1;
+    }
+
+    printf("wyniki  = {%d,%d,%d,%d,%d}\n",
+        c[0], c[1], c[2], c[3], c[4]);
+
+    // cudaDeviceReset must be called before exiting in order for profiling and
+    // tracing tools such as Nsight and Visual Profiler to show complete traces.
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+    getchar();
+    return 0;
+}
+
+// Helper function for using CUDA to add vectors in parallel.
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
+{
+    int *dev_a = 0;
+    int *dev_b = 0;
+    int *dev_c = 0;
+    cudaError_t cudaStatus;
+    cudaEvent_t start, stop;
+    clock_t startt, stopt;
+    float msecTotal = 0.0f;
+    double gigaFlops = 0;
+    
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+   cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+    
+    cudaStatus = cudaEventCreate(&start);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaEventCreate failed!");
+        goto Error;
+    }
+    cudaStatus = cudaEventCreate(&stop);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaEventCreate failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaEventRecord(start, 0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaEventRecord failed");
+        goto Error;
+    }
+    startt = clock();//jeszcze obliczenia się nie zaczęły
+
+    addKernel<<<1000000, size>>>(dev_c, dev_a, dev_b);
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+       
+    cudaStatus = cudaEventRecord(stop, 0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaEventRecord failed");
+        goto Error;
+    }
+   
+    cudaStatus = cudaEventSynchronize(stop);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaEventSynchronize(stop) failed");
+        goto Error;
+    }
+    stopt = clock();//obliczenia już zakończone
+
+    printf("Czas przetwarzania wynosi %.3f msekund\n", ((double)(stopt - startt)));
+
+    
+    cudaEventElapsedTime(&msecTotal, start, stop);
+    //obliczenia predkości obliczeń 
+    gigaFlops = 0;//  (liczba operacji arytmetycznych) / (msecTotal / 1000.0f); */
+    printf(
+        "Performance= %.2f GFlop/s, Time= %.3f msec \n",
+        gigaFlops, msecTotal);
+   
+    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    
+    return cudaStatus;
+}

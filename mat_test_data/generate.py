d46dc0048ec1bb4e04e4c2467c83f0b9fd5b7003
inf160465 <franciszek.kaczmarek@student.put.poznan.pl>
2026-06-08 18:49:15 +0200

    big changes

--- mat_test_data/generate.py
+++
@@ -1,9 +0,0 @@
-import random
-files = ["matA.bin", "matB.bin"]
-
-for  file in files:
-    with open(file, "wb") as f:
-        size = 3200
-        for i in range (size):
-            for j in range (size):
-                f.write(random.randint(1, 5).to_bytes(1, byteorder='little'))

570f9bbabfd47cf2afd66e41ceb7af6d390a0d4e
Kamil <kamil.suwiczak@wp.pl>
2026-06-08 15:38:55 +0200

    update generate and calc

--- mat_test_data/calc.py
+++ mat_test_data/calc.py
@@ -1,11 +1,25 @@
 import numpy as np
 
 def read_matrice(filename):
-    return np.loadtxt(filename, dtype=int)
+    return np.fromfile(filename, dtype=np.int8).reshape(3200, 3200)
 
-A = read_matrice("matA.txt")
-B = read_matrice("matB.txt")
+A = read_matrice("matA.bin")
+B = read_matrice("matB.bin")
+
+print("Matrix A:")
+print(A)  
+print(A.shape)
+print("Matrix B:")
+print(B)
+print(B.shape)  
+
+
+A = A.astype(np.int32)
+B = B.astype(np.int32)
+
 result = np.matmul(A, B)
+result.tofile("result_A_B.bin")
 
-# np.savetxt("result_A_B.txt", result, fmt="%d")
-np.save("result_A_B.npy", result)
+
+# np.savetxt("result_A_B.txt", result, fmt="%d",encoding="binary")
+# np.save("result_A_B.npy", result)
--- mat_test_data/generate.py
+++ mat_test_data/generate.py
@@ -1,9 +1,9 @@
 import random
+files = ["matA.bin", "matB.bin"]
 
-with open("matB.txt", "w") as f:
-    
-    size = 3200
-    for i in range (size):
-        for j in range (size):
-            f.write(str(random.randint(1, 5))+ " ")
-        f.write("\n")
+for  file in files:
+    with open(file, "wb") as f:
+        size = 3200
+        for i in range (size):
+            for j in range (size):
+                f.write(random.randint(1, 5).to_bytes(1, byteorder='little'))
